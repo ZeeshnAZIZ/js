56ad6d2bc7957cbe020f75d7a6ee1a4d1f6f076a
zeeshan <zeeshanaziz006@gmail.com>
2024-02-03 18:01:22 +0500

    html

---
+++ comaprison.js
@@ -0,0 +1,20 @@
+//console.log(2>1)
+//console.log(2<=1)
+
+
+//console.log("2">1) is always true
+
+//console.log(null>0) false
+
+console.log(null >= 0)
+// is true because null is 0
+
+console.log(undefined>0)
+
+
+// === value check strictly 
+
+
+ // clean code first pirotiry in coding
+
+ 
